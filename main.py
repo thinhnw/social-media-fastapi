0f442872a0479adb3c00340aafc7ee726fac5433
thinhnw <thinhnv0412@gmail.com>
2024-09-18 18:06:38 +0700

    Add Post schema and routes

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from schemas import Post
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
     return {"Ping": "Pong"}
+
+
+@app.get("/posts")
+async def get_posts():
+    return {"data": "These are your posts"}
+
+
+@app.post("/posts")
+async def create_post(payload: Post):
+    return payload
