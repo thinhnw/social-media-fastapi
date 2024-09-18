21f360d0afdfe77f3b0116b9efa1d0a2e1c188bd
thinhnw <thinhnv0412@gmail.com>
2024-09-18 19:23:41 +0700

    Finish RESTful Posts API

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Response, status, HTTPException
 from schemas import Post
 from database import db
 
@@ -15,9 +15,12 @@ async def get_posts():
 
 
 @app.get("/posts/{id}")
-async def get_post(id: int):
-    post = next((post for post in db["posts"] if post["id"] == id), None)  
-    return post if post else {"error": "Post not found"} 
+async def get_post(id: int, response: Response):
+    post = next((post for post in db["posts"] if post["id"] == id), None)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
+    return { "post": post }
 
 
 @app.post("/posts")
@@ -28,3 +31,31 @@ async def create_post(payload: Post):
         "id": id
     })
     return db["posts"][-1]
+
+
+@app.delete("/posts/{id}")
+async def delete_post(id: int):
+    post = next((post for post in db["posts"] if post["id"] == id), None)
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
+    db["posts"].remove(post)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+
+@app.put("/posts/{id}")
+async def update_post(id: int, post: Post):
+    index = next(
+        (index for index, p in enumerate(db["posts"]) if p["id"] == id), 
+        None
+    )
+    if index is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Post with id: {id} not found")
+    db["posts"][index] = {
+        **db["posts"][index],
+        **post.model_dump()
+    }
+    return {
+        "data": db["posts"][index]
+    }
