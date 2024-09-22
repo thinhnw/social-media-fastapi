2576e3f1012a6f3d397fc88a93879ff1129cd30c
thinhnw <thinhnv0412@gmail.com>
2024-09-23 00:29:54 +0700

    Change some configs

--- app/config.py
+++ app/config.py
@@ -2,8 +2,22 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     api_url: str
-    database_connection_str: str
-    testdb_connection_str: str
+    postgres_user: str  
+    postgres_password: str
+    postgres_host: str
+    postgres_port: str
+    postgres_db: str
+    @property
+    def database_connection_str(self) -> str: 
+        return (
+            "postgresql+psycopg://"
+            f"{self.postgres_user}:{self.postgres_password}"
+            f"@{self.postgres_host}:{self.postgres_port}/{self.postgres_db}"
+        )
+    @property
+    def testdb_connection_str(self) -> str: 
+        return self.database_connection_str + "_test"
+
     jwt_secret_key: str
     jwt_algorithm: str = "HS256" 
     jwt_token_ttl: int = 60 * 60
